6d591e6c396dbc85ea8db95dd59f6a32f263204f
Maxime Engels <maxime.engels@student.ehb.be>
2020-10-04 17:26:12 +0200

    React to do working, added framer motion

--- react-prototype/src/App.js
+++ react-prototype/src/App.js
@@ -1,10 +1,90 @@
 import React from 'react';
 import './App.css';
+import { motion } from "framer-motion";
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      {todo.text}
+      <div className="buttons">
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        placeholder="New item here"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React",
+    isCompleted: false },
+    { text: "Meet friend for lunch",
+    isCompleted: false },
+    { text: "Build really cool todo app",
+    isCompleted: false }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="wrapper">
+        <div className="headers">
+          <h1>Hi, this is a to-do list.</h1>
+          <h3>Now delete everything and do whatever :)</h3>
+        </div>
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+        <footer><p>A Framer Motion and React prototype</p></footer>
+      </div>
     </div>
   );
 }
