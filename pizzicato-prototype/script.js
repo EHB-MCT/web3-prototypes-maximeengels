886616f16617c850e4eb69ab59d811f117305257
Maxime Engels <maxime.engels@student.ehb.be>
2020-10-07 17:23:37 +0200

    Sound effect added

--- pizzicato-prototype/script.js
+++ pizzicato-prototype/script.js
@@ -1,14 +1,43 @@
+var volumeValue = document.querySelector("#volume");
+var mixValue = document.querySelector("#convolver-mix");
+var volumeText = document.querySelector(".volText");
+var mixText = document.querySelector(".mixText");
 
-var sound = new Pizzicato.Sound('assets/audio/sound.mp3', function() {
-  // Sound loaded!
-  // sound.play();
+
+var sound = new Pizzicato.Sound({
+  source: 'file',
+  options: { 
+    path: 'assets/audio/drums.mp3',
+    loop: 'true'}
+}, function() {
+  console.log("Song file loaded!");
 });
 
 var convolver = new Pizzicato.Effects.Convolver({
-  impulse: 'assets/audio/sound.mp3',
-  volume: 0.5,
-  mix: 0.5
+  impulse: 'assets/audio/scala-milan.wav',
+  mix: mixValue.value
 }, function (error) {
-  sound.addEffect(convolver);
-  // sound.play();
 });
+sound.addEffect(convolver);
+
+volumeValue.oninput = function(){
+  console.log(this.value);
+  sound.volume = this.value;
+  volumeText.innerHTML = this.value;
+}
+
+mixValue.oninput = function(){
+  mixText.innerHTML = this.value;
+}
+
+function playMusic(){
+  sound.play();
+}
+
+function pauseMusic(){
+  sound.pause();
+}
+
+function stopMusic(){
+  sound.stop();
+}
