d251b6fd8df9c9ef835b67aa9729f7a03f6dadc5
Maxime Engels <maxime.engels@student.ehb.be>
2020-11-02 11:47:59 +0100

    ReactHowler package

--- task6-challenge-prototype/src/components/DetailHowler.js
+++ task6-challenge-prototype/src/components/DetailHowler.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactHowler from 'react-howler';
 import useFirestore from '../hooks/useFirestore';
 
 const DetailHowler = ({ setSelectedImg }) => {
@@ -6,17 +7,21 @@ const DetailHowler = ({ setSelectedImg }) => {
   const { docs } = useFirestore('uploads');
 
   return (
-    <div className="uploadGrid">
-      {docs && docs.map(doc => (
-      <div key={doc.id} id={doc.id} className="tile">
-        <h3 className="challengeTitle">{doc.name}</h3>
-        <p>Made with FL Studio</p>
-          <audio controls>
-            <source src={doc.url} type="audio/mpeg"/>
-          </audio>
-      </div>
-      ))}
-    </div>
+    // <div className="uploadGrid">
+    //   {docs && docs.map(doc => (
+    //   <div key={doc.id} id={doc.id} className="tile">
+    //     <h3 className="challengeTitle">{doc.name}</h3>
+    //     <p>Made with FL Studio</p>
+    //       <audio controls>
+    //         <source src={doc.url} type="audio/mpeg"/>
+    //       </audio>
+    //   </div>
+    //   ))}
+    // </div>
+    <ReactHowler
+        src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
+        playing={true}
+      />
   )
 }
 
